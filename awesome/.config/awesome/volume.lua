3bd34981886dc55e33eb5cb350170edad812cb57
Ludovic Chopin <lchopin+github@gmail.com>
2016-01-26 11:04:12 +0100

    awesome: get real sink index

--- awesome/.config/awesome/volume.lua
+++ awesome/.config/awesome/volume.lua
@@ -8,7 +8,7 @@ local awful = require("awful")
 require("autostart")
 -- Get the sink index. https://wiki.archlinux.org/index.php/PulseAudio/Examples
 local fd = io.popen("LANG=C pacmd list-sinks | grep '* index' | awk '{print $3}'")
-local sink = tonumber(fd:read("*all"))
+local default_sink = tonumber(fd:read("*all"))
 fd:close()
 
 -- Color constants
@@ -18,24 +18,28 @@ local mute_color = '#cc3333'
 local background_color = '#222222'
 local background_over_100_color = normal_color
 
-local nb_sink = 0
+local sink_tab = {} -- new array with sinks index
 
 local function refresh_sinks()
-    local fd = io.popen("LANG=C pacmd list-sinks | grep index | wc -l")
-    nb_sink = tonumber(fd:read("*all"))
+    local fd = io.popen("LANG=C pacmd list-sinks | grep index | grep -o '[0-9]*'")
+    local i = 1 -- However, it is customary in Lua to start arrays with index 1 http://www.lua.org/pil/11.1.html
+    for line in fd:lines() do
+        sink_tab[i] = tonumber(line)
+        i = i + 1
+    end
     fd:close()
 end
 
 -- Functions to fetch volume information (pulseaudio)
 function get_volume() -- returns the volume as a float (1.0 = 100%)
-    local fd = io.popen("LANG=C pactl list | grep -A 9001 'Sink #".. sink .."' | grep Volume | head -n 1 |  grep -o '...%' | head -n1 | sed 's/[^0-9]*//g'")
+    local fd = io.popen("LANG=C pactl list | grep -A 9001 'Sink #".. default_sink .."' | grep Volume | head -n 1 |  grep -o '...%' | head -n1 | sed 's/[^0-9]*//g'")
     local volume_str = fd:read("*all")
     fd:close()
     return tonumber(volume_str) / 100
 end
 
 function get_mute() -- returns a true value if muted or a false value if not
-    fd = io.popen("LANG=C pactl list | grep -A 9001 'Sink #".. sink .."' | grep Mute | head -n 1")
+    fd = io.popen("LANG=C pactl list | grep -A 9001 'Sink #".. default_sink .."' | grep Mute | head -n 1")
     local mute_str = fd:read("*all")
     fd:close()
     return string.find(mute_str, "yes")
@@ -65,23 +69,23 @@ end
 -- Volume control functions for external use
 function inc_volume(widget)
     -- awful.util.spawn("amixer -D pulse set Master 5%+") 
-    for s = 0, nb_sink do
-        awful.util.spawn("pactl -- set-sink-volume ".. s .." +3%", false)
+    for k,v in pairs(sink_tab) do
+        awful.util.spawn("pactl -- set-sink-volume ".. v .." +3%", false)
     end
     update_volume(widget)
 end
 
 function dec_volume(widget)
-    for s = 0, nb_sink do
-        awful.util.spawn("pactl -- set-sink-volume ".. s .." -3%", false)
+    for k,v in pairs(sink_tab) do
+        awful.util.spawn("pactl -- set-sink-volume ".. v .." -3%", false)
     end
     update_volume(widget)
 end
 
 function mute_volume(widget)
     refresh_sinks()
-    for s = 0, nb_sink do
-        awful.util.spawn("pactl -- set-sink-mute ".. s .." toggle", false)
+    for k,v in pairs(sink_tab) do
+        awful.util.spawn("pactl -- set-sink-mute ".. v .." toggle", false)
     end
     update_volume(widget)
 end
