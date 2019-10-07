6f26887fee86b8847dad954e01c7303ca5eca70b
Ludovic Chopin <lchopin+github@gmail.com>
2019-10-07 08:29:09 +0200

    awesome: remove function that is not compatible with awesome 4.3

--- awesome/.config/awesome/utils.lua
+++ awesome/.config/awesome/utils.lua
@@ -1,18 +1,5 @@
 local naughty = require("naughty")
 
--- http://awesome.naquadah.narkive.com/f5MrgAjx/zap-naughty-notifications
--- destroy notifications by keys {{{
-function close_last_naughty_msg()
-    screen = naughty.config.defaults.screen
-    for p,pos in pairs(naughty.notifications[screen]) do
-        for i,n in pairs(naughty.notifications[screen][p]) do
-            naughty.destroy(n)
-            return true
-        end
-    end
-    return false
-end
-
 function close_all_naughty_msg()
 	naughty.destroy_all_notifications()
 end
