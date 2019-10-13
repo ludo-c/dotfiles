342eff978ec9d09b39e828ded3e00e3176a1b4f6
Ludovic Chopin <lchopin+github@gmail.com>
2019-10-13 17:54:16 +0200

    awesome: improve detection of used network interfaces

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -267,7 +267,7 @@ vicious.register(mytextclock, vicious.widgets.date, " %a %b %d, %R")
 eths = {}
 function update_eths()
 	-- https://unix.stackexchange.com/questions/270008/retrieve-name-of-the-active-network-interface-only
-	awful.spawn.easy_async_with_shell("LANG=C ip addr show | awk '/inet.*brd/{print $NF}'", function(stdout)
+	awful.spawn.easy_async_with_shell("LANG=C ip addr show | awk '/inet.*(brd|peer)/{print $NF}'", function(stdout)
 	    local i = 1 -- However, it is customary in Lua to start arrays with index 1 http://www.lua.org/pil/11.1.html
 		for line in stdout:gmatch"(.-)\n" do
             eths[i] = line
