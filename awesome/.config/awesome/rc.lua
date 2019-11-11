344e9cab982643b6a6790181c3b28295c7a03e3d
Ludovic Chopin <lchopin+github@gmail.com>
2019-11-11 14:24:42 +0100

    awesome: reset eth tab in the callback

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -267,9 +267,9 @@ vicious.register(mytextclock, vicious.widgets.date, " %a %b %d, %R")
 eths = {}
 function update_eths()
 	-- https://unix.stackexchange.com/questions/270008/retrieve-name-of-the-active-network-interface-only
-	eths = {}  -- reset tab to avoid double wifi interface
 	awful.spawn.easy_async_with_shell("LANG=C ip addr show | awk '/inet.*(brd|peer)/{print $NF}'", function(stdout)
 	    local i = 1 -- However, it is customary in Lua to start arrays with index 1 http://www.lua.org/pil/11.1.html
+        eths = {}  -- reset tab to avoid double wifi interface
 		for line in stdout:gmatch"(.-)\n" do
             eths[i] = line
             i = i + 1
